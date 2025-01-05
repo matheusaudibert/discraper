eddd98e4e0452712521fc7bd9e05904e96cdaa0a
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-01-05 01:29:20 -0300

    fix: domain

--- src/public/scripts/serverInfo.js
+++ src/public/scripts/serverInfo.js
@@ -1,7 +1,6 @@
-const PORT = document.querySelector('meta[name="port"]')?.content || 5500;
-
 async function getServerInfo(token, guildId) {
-    const response = await fetch(`http://localhost:${PORT}/server-info`, {
+    const currentOrigin = window.location.origin;
+    const response = await fetch(`${currentOrigin}/server-info`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
