a4fa12872af28af053e32647c6ba226bbc6ccaaa
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-01-28 17:01:17 -0300

    selfbot fix

--- src/server/serverInfo.js
+++ src/server/serverInfo.js
@@ -1,49 +1,51 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Discord = require('discord.js-selfbot-v13');
-const cors = require('cors');
+const Discord = require("discord.js-selfbot-v13");
+const cors = require("cors");
 
 router.use(cors());
 
-router.post('/server-info', async (req, res) => {
-    const { token, guildId } = req.body;
-    const client = new Discord.Client();
-    
-    try {
-        await client.login(token);
-        
-        client.on('ready', async () => {
-            console.log(`${client.user.tag} is online!`);
-            try {
-                const TARGET_DATE = new Date('2018-10-10');
-                const guild = await client.guilds.fetch(guildId);
-                const totalMembers = await guild.members.fetch();
-                const totalMembersCount = totalMembers.size;
+router.post("/server-info", async (req, res) => {
+  const { token, guildId } = req.body;
+  const client = new Discord.Client();
 
-                const membersToFetch = totalMembers.filter(member => member.user.createdAt < TARGET_DATE);
-                const membersToFetchCount = membersToFetch.size;
-                const membersToFetchIds = membersToFetch.map(member => member.id);
+  try {
+    await client.login(token);
 
-                const serverName = guild.name;
-                const serverIconUrl = guild.iconURL({ format: 'png', dynamic: true });
+    client.on("ready", async () => {
+      console.log(`${client.user.tag} is online!`);
+      try {
+        const TARGET_DATE = new Date("2018-10-10");
+        const guild = await client.guilds.fetch(guildId);
+        const totalMembers = await guild.members.fetch();
+        const totalMembersCount = totalMembers.size;
 
-                res.json({
-                    serverName,
-                    serverIconUrl,
-                    totalMembersCount,
-                    membersToFetchCount,
-                    membersToFetchIds
-                });
-            } catch (error) {
-                res.status(500).json({ error: error.message });
-            } finally {
-                console.log(`${client.user.tag} is offline!`);
-                client.destroy();
-            }
+        const membersToFetch = totalMembers.filter(
+          (member) => member.user.createdAt < TARGET_DATE
+        );
+        const membersToFetchCount = membersToFetch.size;
+        const membersToFetchIds = membersToFetch.map((member) => member.id);
+
+        const serverName = guild.name;
+        const serverIconUrl = guild.iconURL({ format: "png", dynamic: true });
+
+        res.json({
+          serverName,
+          serverIconUrl,
+          totalMembersCount,
+          membersToFetchCount,
+          membersToFetchIds,
         });
-    } catch (error) {
+      } catch (error) {
         res.status(500).json({ error: error.message });
-    }
+      } finally {
+        console.log(`${client.user.tag} is offline!`);
+        client.destroy();
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = router;
