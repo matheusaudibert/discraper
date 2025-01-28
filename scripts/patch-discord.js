c7092514462c795ce39fa8416ee421eb6e8e837d
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-01-28 18:10:51 -0300

    slefbot-v13 fix

--- scripts/patch-discord.js
+++ scripts/patch-discord.js
@@ -12,10 +12,27 @@ fs.readFile(filePath, "utf8", (err, data) => {
     return;
   }
 
-  const patchedData = data.replace(
-    "all: data.friend_source_flags.all || false,",
-    "all: (data.friend_source_flags && data.friend_source_flags.all) || false,"
-  );
+  const patchedData = data
+    .replace(
+      "all: data.friend_source_flags.all || false,",
+      "all: (data.friend_source_flags && data.friend_source_flags.all) || false,"
+    )
+    .replace(
+      "mutual_friends: data.friend_source_flags.mutual_friends || false,",
+      "mutual_friends: (data.friend_source_flags && data.friend_source_flags.mutual_friends) || false,"
+    )
+    .replace(
+      "mutual_guilds: data.friend_source_flags.mutual_guilds || false,",
+      "mutual_guilds: (data.friend_source_flags && data.friend_source_flags.mutual_guilds) || false,"
+    )
+    .replace(
+      "mutual_friends: data.friend_source_flags.all ? true : data.friend_source_flags.mutual_friends,",
+      "mutual_friends: (data.friend_source_flags && data.friend_source_flags.all) ? true : (data.friend_source_flags && data.friend_source_flags.mutual_friends),"
+    )
+    .replace(
+      "mutual_guilds: data.friend_source_flags.all ? true : data.friend_source_flags.mutual_guilds,",
+      "mutual_guilds: (data.friend_source_flags && data.friend_source_flags.all) ? true : (data.friend_source_flags && data.friend_source_flags.mutual_guilds),"
+    );
 
   fs.writeFile(filePath, patchedData, "utf8", (err) => {
     if (err) {
