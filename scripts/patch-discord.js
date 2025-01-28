2926cae0bd7b1e266281c0ad20af6930207acea8
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-01-28 17:45:05 -0300

    patch

--- scripts/patch-discord.js
+++ scripts/patch-discord.js
@@ -1,25 +1,28 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const filePath = path.join(__dirname, '../node_modules/discord.js-selfbot-v13/src/managers/ClientUserSettingManager.js');
+const filePath = path.join(
+  __dirname,
+  "../node_modules/discord.js-selfbot-v13/src/managers/ClientUserSettingManager.js"
+);
 
-fs.readFile(filePath, 'utf8', (err, data) => {
+fs.readFile(filePath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error", err);
+    return;
+  }
+
+  const patchedData = data.replace(
+    "all: data.friend_source_flags.all || false,",
+    "all: (data.friend_source_flags && data.friend_source_flags.all) || false,"
+  );
+
+  fs.writeFile(filePath, patchedData, "utf8", (err) => {
     if (err) {
-        console.error('Erro ao ler o arquivo:', err);
-        return;
+      console.error("Error:", err);
+      return;
     }
 
-    const patchedData = data.replace(
-        'all: data.friend_source_flags.all || false,',
-        'all: (data.friend_source_flags && data.friend_source_flags.all) || false,'
-    );
-
-    fs.writeFile(filePath, patchedData, 'utf8', (err) => {
-        if (err) {
-            console.error('Erro ao escrever o arquivo:', err);
-            return;
-        }
-
-        console.log('Arquivo patchado com sucesso!');
-    });
+    console.log("Patched!");
+  });
 });
